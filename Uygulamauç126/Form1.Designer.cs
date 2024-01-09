bb8fbbe348c41ad93561d5eddaf3cbbbea4e4e80
eren595 <eren595@BPC12>
2024-01-09 10:47:19 +0300

    Proje dosyası ekle.

---
+++ Uygulamauç126/Form1.Designer.cs
@@ -0,0 +1,130 @@
+﻿namespace Uygulamauç126
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnListe = new System.Windows.Forms.Button();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.txtDersNotu = new System.Windows.Forms.TextBox();
+            this.lbListe = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(192, 60);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(75, 23);
+            this.btnEkle.TabIndex = 0;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnListe
+            // 
+            this.btnListe.Location = new System.Drawing.Point(594, 63);
+            this.btnListe.Name = "btnListe";
+            this.btnListe.Size = new System.Drawing.Size(75, 23);
+            this.btnListe.TabIndex = 1;
+            this.btnListe.Text = "listele";
+            this.btnListe.UseVisualStyleBackColor = true;
+            this.btnListe.Click += new System.EventHandler(this.btnListe_Click);
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(86, 63);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(100, 20);
+            this.txtAdSoyad.TabIndex = 2;
+            // 
+            // txtDersNotu
+            // 
+            this.txtDersNotu.Location = new System.Drawing.Point(86, 118);
+            this.txtDersNotu.Name = "txtDersNotu";
+            this.txtDersNotu.Size = new System.Drawing.Size(100, 20);
+            this.txtDersNotu.TabIndex = 3;
+            // 
+            // lbListe
+            // 
+            this.lbListe.FormattingEnabled = true;
+            this.lbListe.Location = new System.Drawing.Point(468, 63);
+            this.lbListe.Name = "lbListe";
+            this.lbListe.Size = new System.Drawing.Size(120, 95);
+            this.lbListe.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 68);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Ad Soyad:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Ders Notu:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lbListe);
+            this.Controls.Add(this.txtDersNotu);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.btnListe);
+            this.Controls.Add(this.btnEkle);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnListe;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.TextBox txtDersNotu;
+        private System.Windows.Forms.ListBox lbListe;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Uygulamauç126/Form1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Uygulamauç126
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string[] isimrel = new string[5];
+        int [] notlar = new int[5];
+        int index = 0;
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (index < isimrel.Length)
+            {
+                isimrel[index] = txtAdSoyad.Text;
+                notlar[index] = int.Parse(txtDersNotu.Text);
+                index++;
+                txtAdSoyad.Text = "";
+                txtDersNotu.Text = "";
+            }
+        }
+
+        private void btnListe_Click(object sender, EventArgs e)
+        {
+            
+            for (int i = 0; i < isimrel.Length; i++)
+            {
+                if (isimrel[i]!=null)
+                    lbListe.Items.Add(isimrel[i]+" > " + notlar[i]);
+            }
+        }
+    }
+}
